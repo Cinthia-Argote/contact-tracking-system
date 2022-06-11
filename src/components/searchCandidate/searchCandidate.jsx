59e528b9675f8674202b0b102828cd2077342855
cinthia.argote <cinthia.argote1998@gmail.com>
2022-06-11 00:22:12 -0400

    Updating search component

--- src/components/searchCandidate/searchCandidate.jsx
+++ src/components/searchCandidate/searchCandidate.jsx
@@ -1,4 +1,7 @@
 import { Input } from "antd";
+import debounce from "lodash.debounce";
+import { useEffect, useMemo } from "react";
+import { useSelector } from "react-redux";
 
 import useSearchCandidates from "../../hooks/useSearchCandidates";
 
@@ -6,17 +9,31 @@ const { Search } = Input;
 
 const SearchCandidate = () => {
   const searchCandidates = useSearchCandidates();
+  const { loading } = useSelector((state) => state.candidate);
 
-  const handleSearch = (value) => {
+  const handleChange = (event) => {
+    const { target } = event;
+    const { value } = target;
     searchCandidates(value);
   };
+
+  const debouncedResults = useMemo(() => {
+    return debounce(handleChange, 300);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      debouncedResults.cancel();
+    };
+  });
 
   return (
     <Search
       placeholder="Search..."
-      loading={false}
-      onSearch={handleSearch}
+      loading={loading}
       allowClear
+      onChange={debouncedResults}
     />
   );
 };
--- src/hooks/useSearchCandidates.js
+++ src/hooks/useSearchCandidates.js
@@ -1,16 +1,18 @@
 import { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { searchCandidates } from "../services/candidates";
-import { candidateList } from "../store/candidateSlice";
+import { candidateList, loading } from "../store/candidateSlice";
 
 const useSearchCandidates = () => {
   const dispatch = useDispatch();
   const { searchBy } = useSelector((state) => state.candidate);
 
   return useCallback((value) => {
+      dispatch(loading(true));
       searchCandidates(value, searchBy)
       .then(data => {
         dispatch(candidateList(data));
+        dispatch(loading(false));
       });
   }, [dispatch, searchBy]);
 };
